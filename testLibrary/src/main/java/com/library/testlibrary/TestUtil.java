2e17b0001285ca0f4c4bab0dbf675f88237f857c
刘凯 <kailiu@epochcredit.com>
2021-03-25 18:05:05 +0800

    Initial commit

---
+++ testLibrary/src/main/java/com/library/testlibrary/TestUtil.java
@@ -0,0 +1,19 @@
+package com.library.testlibrary;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @Author: Administrator
+ * @E-mail: liukai@duandai.com
+ * @CreateDate: 2021/3/25 18:02
+ * @Description:
+ */
+public class TestUtil {
+
+    public static void test(Context context, String string) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+        Log.e("TAG", "test: " + string);
+    }
+}
