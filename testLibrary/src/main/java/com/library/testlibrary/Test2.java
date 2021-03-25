53ed933924dbd520d294be042b6060438d32cbc9
刘凯 <kailiu@epochcredit.com>
2021-03-25 18:15:49 +0800

    test2

---
+++ testLibrary/src/main/java/com/library/testlibrary/Test2.java
@@ -0,0 +1,16 @@
+package com.library.testlibrary;
+
+import android.util.Log;
+
+/**
+ * @Author: Administrator
+ * @E-mail: liukai@duandai.com
+ * @CreateDate: 2021/3/25 18:14
+ * @Description:
+ */
+public class Test2 {
+
+    public static void test2(String string) {
+        Log.e("TAG", "test2: " + string);
+    }
+}
