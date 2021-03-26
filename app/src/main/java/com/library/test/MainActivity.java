e6e646be4b4387a90b9c541565b744cace73735a
刘凯 <kailiu@epochcredit.com>
2021-03-26 11:00:02 +0800

    app test1.0.2

--- app/src/main/java/com/library/test/MainActivity.java
+++ app/src/main/java/com/library/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.library.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.library.testlibrary.Test2;
 import com.library.testlibrary.TestUtil;
@@ -13,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TestUtil.test(this, "MainActivity--->");
+        TestUtil.test(this, "MainActivity--->toast");
         Test2.test2("test2.....");
+    }
+
+    public void startAct(View view) {
+        int id = view.getId();
+        Log.e("TAG", "id: " + id);
+        TestUtil.startAct(this, "MainActivity open activity");
     }
 }
